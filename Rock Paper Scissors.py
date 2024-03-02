ca46d01497669e2e889a9a8d2685de336b72c4fe
CODE_BRV <69113431+CODEBRV@users.noreply.github.com>
2024-03-02 18:43:29 +0530

    Add files via upload

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,93 @@
+from tkinter import * 
+import random  as rn
+
+window=Tk()
+
+window.title("Rock Paper Scissors")
+window.geometry("1280x720")
+
+usr=0
+comp=0
+
+def rand():
+    return rn.choice(["rock", "paper", "scissor"])
+def update():
+    l2.config(text=f"You: {usr}")
+    l3.config(text=f"Computer: {comp}")
+
+def rock():
+    global usr
+    global comp
+    print("rock")
+    temp=rand()
+    l4.pack_forget()
+    l5.pack_forget()
+    if temp=="rock":
+        comp=comp+1
+        print("comp")
+        l5.pack()
+    else:
+        usr=usr+1
+        print("usr")
+        l4.pack()
+
+    update()
+
+def paper():
+    global usr
+    global comp
+    print("paper")
+    temp=rand()
+    l4.pack_forget()
+    l5.pack_forget()
+    if temp=="paper":
+        comp=comp+1
+        print("comp")
+        l5.pack()
+    else:
+        usr=usr+1
+        print("usr")
+        l4.pack()
+
+    update()
+
+def scissor():
+    global usr
+    global comp
+    print("scissor")
+    temp=rand()
+    l4.pack_forget()
+    l5.pack_forget()
+    if temp=="scissor":
+        comp=comp+1
+        print("comp")
+        l5.pack()
+    else:
+        usr=usr+1
+        print("usr")
+        l4.pack()
+
+    update()
+    
+f1=Frame(window)
+f1.pack()
+l2=Label(f1, text=f"You: {usr}",font="AnnaiMN 30")
+l2.pack(side='left')
+l1=Label(f1,text="Rock Paper Scissors",font="AnnaiMN 40")
+l1.pack(side='left',padx=300)
+l3=Label(f1, text=f"Computer: {comp}",font="AnnaiMN 30")
+l3.pack(side='left')
+
+f2=Frame(window)
+f2.pack(pady= 150)
+b1=Button(f2, text="Rock", command=rock,font="AnnaiMN 30")
+b1.pack(side='left')
+b2=Button(f2, text="Paper", command=paper,font="AnnaiMN 30")
+b2.pack(side='left',padx=300)
+b3=Button(f2, text="Scissor", command=scissor,font="AnnaiMN 30")
+b3.pack(side='left')
+
+l4=Label(window,text="🎉YOU WIN🎉",font="AnnaiMN 30")
+l5=Label(window,text="🎉COMPUTER WINS🎉",font="AnnaiMN 30")
+
+window.mainloop()
